bed41565acf9bebdedf5eb993c67ce199f53facd
ShubhamPatel <patelshubham8249@gmail.com>
2023-12-10 00:15:05 -0500

    first commit

---
+++ CJS/index.js
@@ -0,0 +1,5 @@
+import { calculateArea, calculateVolume, calculateSquare } = require('./calculate');
+
+console.log(calculateArea(7, 7));
+console.log(calculateVolume(7, 7, 7));
+console.log(calculateSquare(7));
---
+++ EJS/index.js
@@ -0,0 +1,5 @@
+import { calculateArea, calculateVolume, calculateSquare } from './calculate.mjs';
+
+console.log(calculateArea(7, 7));
+console.log(calculateVolume(7, 7, 7));
+console.log(calculateSquare(7));
